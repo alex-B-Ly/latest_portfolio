4874d27e4c2ac54ea26ee66263bdf430b33b5949
alex-B-Ly <abl12582@gmail.com>
2016-05-20 01:49:24 -0400

    Returning JSON into document

--- static/js/functionality.js
+++ static/js/functionality.js
@@ -7,5 +7,11 @@ $(document).ready(function(){
   $('.dropdown-button').dropdown();
 
   // Portfolio
-
+  $('#test').on('click', function(){
+  	$.getJSON('http://localhost/sites/latest_portfolio/static/js/info/projects.json', function(data){
+  		for(var i=0; i<data.length; i++){
+  			console.log(data[i]);
+  		}
+  	});
+  });
 });
