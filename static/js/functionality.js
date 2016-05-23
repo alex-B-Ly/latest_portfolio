d33fd396c43b2ff411d5fe944c243812dc7c2f0e
alex-B-Ly <abl12582@gmail.com>
2016-05-23 03:35:36 -0400

    Passing object data to projects

--- static/js/functionality.js
+++ static/js/functionality.js
@@ -10,22 +10,27 @@ $(document).ready(function(){
   function projects(){
   	$.getJSON('http://localhost/sites/latest_portfolio/static/js/info/projects.json', function(data){
   		$(data).each(function(i, val){
-  			console.log(val);
   			projectCreator(val);
   		});
   	});
 
   	function projectCreator(project){
   		var newProject = $('<div>').addClass('col s10 offset-s1 card-panel hoverable center-align');
+  		var proj = $('<div>').addClass('project').data(project);
   		var title = $('<h3>').text(project.title);
   		var techTitle = $('<h5>').text('Tech Used:');
   		var descTitle = $('<h5>').text('Description:');
   		var description = $('<p>').text(project.description);
   		var pic = $('<img>').addClass('responsive-img').attr('src', project.picture_url);
-  		newProject.append(pic).append(title).append(techTitle).append(descTitle).append(description);
+  		proj.append(pic).append(title).append(techTitle).append(descTitle).append(description);
+  		newProject.append(proj);
   		$('#portfolio').append(newProject);
   	}
   }
+
+  $(document).on('click', '.project', function(){
+  	console.log($(this).data());
+  });
 
   // FUNCTIONS CALLED ON LOAD
   projects();
